ce3147a340cb67f59f3c190ffae3da7aa6b5a8d1
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-05-26 22:57:34 +0300

    Web: Strarted web module. Added ScalaJS plugin to build.sbt and plugins.sbt. Added simple index.html, style.css with some style and simple Main class.

--- web/build.sbt
+++ web/build.sbt
@@ -1,6 +1,9 @@
+enablePlugins(ScalaJSPlugin)
+
 name := "web"
 
 version := "1.0"
 
 scalaVersion := "2.11.11"
-        
+
+scalaJSUseMainModuleInitializer := true
---
+++ web/src/main/scala/com/aleksandrbogomolov/Main.scala
@@ -0,0 +1,8 @@
+package com.aleksandrbogomolov
+
+import scala.scalajs.js.JSApp
+
+object Main extends JSApp {
+
+  override def main(): Unit = println("Hello World!")
+}
