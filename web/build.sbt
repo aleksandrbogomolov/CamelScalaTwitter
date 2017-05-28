750536985eba23172072f7361ff9ee0aff301bc0
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-05-28 08:48:55 +0300

    Web: Added jQuery dependency to the build.sbt, configure JS dependency. Added simple form to the index.html and some style in the style.css. Added alert 'setupUI' method with '#clickMeButton' listener to the Main.

--- web/build.sbt
+++ web/build.sbt
@@ -7,3 +7,12 @@ version := "1.0"
 scalaVersion := "2.11.11"
 
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies ++= Seq(
+  "be.doeraene" % "scalajs-jquery_sjs0.6_2.11" % "0.9.1"
+)
+
+skip in packageJSDependencies := false
+jsDependencies ++= Seq(
+  "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+)
--- web/src/main/scala/com/aleksandrbogomolov/Main.scala
+++ web/src/main/scala/com/aleksandrbogomolov/Main.scala
@@ -1,8 +1,17 @@
 package com.aleksandrbogomolov
 
+import org.scalajs.jquery.jQuery
+
+import scala.scalajs.js.Dynamic.{global => g}
 import scala.scalajs.js.JSApp
 
 object Main extends JSApp {
 
-  override def main(): Unit = println("Hello World!")
+  override def main(): Unit = setupUI()
+
+  def setupUI(): Unit = {
+    jQuery("#clickMeButton").click(() => addClickedMessage())
+  }
+
+  def addClickedMessage(): Unit = g.alert("Start the Twitter Feed!")
 }
